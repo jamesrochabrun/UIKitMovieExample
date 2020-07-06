15eec6ba7102d472028157b316e3b6183b1675ae
james rochabrun <jamesrochabrun@gmail.com>
2020-07-06 16:29:45 -0700

    Initial Commit

---
+++ UIKitMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitMoviesApp
+//
+//  Created by James Rochabrun on 7/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
